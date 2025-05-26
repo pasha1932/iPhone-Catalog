1bc26c62742b625277c43632b4c13ed41fbfb78a
Pavlo Pukalo <misterkatlet@gmail.com>
2025-05-26 16:56:53 +0300

    page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
+import { Outlet } from 'react-router-dom';
+import 'bulma/css/bulma.css';
 import './App.scss';
+import Header from './modules/shared/components/Header/Header';
+import Footer from './modules/shared/components/Footer/Footer';
+import { useAppDispatch } from './api/hooks';
+import { useEffect } from 'react';
+import { setProducts } from './features/products/productsSlice';
+import { setPhones } from './features/phones/phonesSlice';
+import { setTablets } from './features/tablets/tabletsSlice';
+import { setAccessories } from './features/accessories/accessories';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+export const App = () => {
+  const dispatch = useAppDispatch();
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+  useEffect(() => {
+    dispatch(setProducts());
+    dispatch(setPhones());
+    dispatch(setTablets());
+    dispatch(setAccessories());
+  }, [dispatch]);
 
-export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="App">
+      <Header />
+      <main>
+        <div className="container">
+          <Outlet />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 };
+
+// add 5s timer to slide banner and infinity slider +
+// add grid
+// add gap between section +
+// add categories section amount of product
+// add transform to images
+// channge images in main slider
